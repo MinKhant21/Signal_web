24d4a92058f9fa0393ffa7105ae750282c5ec559
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-10-19 16:56:20 +0630

    refactor

---
+++ src/components/Form/ChatInput.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+export default function ChatInput({HandleInput}) {
+    // const [message , setMessage] = useState('')
+    // const HandleEvent = (e) =>{
+    //     setMessage(e.target.value)
+    // }
+  
+  return (
+    <input type="text" placeholder="Message"
+    class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
+    name="message" required onChange={(e)=>HandleInput(e.target.value)}
+    />
+  )
+}
